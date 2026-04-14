6e493abdfc0f82603923feb21dc11544d29ea3c0
dubhaltach-mcs <dubhaltach@lugglass.com>
2026-04-14 14:37:02 +0200

    Updated image_loader to use libraries that are easier to install (i.e. replaced a cv2 function I couldn't get working with matplotlib) and added a folder training_data with one jpeg example

--- image_loader.py
+++ image_loader.py
@@ -1,7 +1,9 @@
 import cv2
 import os
+import matplotlib.pyplot as plt
 
 def load_image(path):
+    """ This reads an image from a path using the library cv2"""
     if not os.path.exists(path):
         print("File not found.")
         return None
@@ -15,14 +17,15 @@ def load_image(path):
     return image
 
 def main():
+    """ This runs the image loader, it loads an image with cv2 and then displays with matplotlib"""
     path = input("Enter path to handwritten image: ")
+
     image = load_image(path)
     
     if image is not None:
         print("Image loaded successfully!")
-        cv2.imshow("Input Image", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        plt.imshow(image, cmap="grey")
+        plt.show()
 
 if __name__ == "__main__":
     main()
